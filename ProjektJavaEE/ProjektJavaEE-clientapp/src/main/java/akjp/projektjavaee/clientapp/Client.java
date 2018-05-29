ecaa0ee690057ed62cb8492f48a379a2881d52fb
Andzik <smers20@wp.pl>
2018-05-29 09:47:45 +0200

    lublacki

---
+++ ProjektJavaEE/ProjektJavaEE-clientapp/src/main/java/akjp/projektjavaee/clientapp/Client.java
@@ -0,0 +1,9 @@
+package akjp.projektjavaee.clientapp;
+
+import javax.naming.NamingException;
+
+public class Client {
+    public static void main(String[] args) throws NamingException {
+        System.out.println("SuperFajerBoy");
+    }
+}
